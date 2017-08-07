e025e8b19f3819596994ba4d68842898578a343d
peorex <30592751+peorex@users.noreply.github.com>
2017-08-07 05:31:00 +0300

    Add files via upload

---
+++ URLs.php
@@ -0,0 +1,37 @@
+<?php
+
+// Version	1.1.5
+
+
+// lower case only!
+$URLs = array (
+//	'name'					=> 'value',						// not set
+	'dbg'					=> 'http://www.dir.bg',					// test
+
+// -------------------------
+	'default' => 'http://stonestreem.com/blog/'		// fallback URL
+); // $URLs = array (...)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
